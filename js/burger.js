9c2e06c61a51ee09652ca617b4a694f96648b24b
Yann Letouzey <y.letouzey@gmail.com>
2023-05-20 13:37:04 +0200

    add img + transition menu

--- js/burger.js
+++ js/burger.js
@@ -1,6 +1,10 @@
 export function burgerActive() {
+    const header__container = document.getElementById('header__container')
     const header__icon = document.getElementById('header__icon')
+    const header__contact = document.getElementById('header__contact')
     header__icon.addEventListener('click', () => {
+        header__container.classList.toggle('is-opened')
         header__icon.classList.toggle('is-opened')
+        header__contact.classList.toggle('is-opened')
     })
 }
