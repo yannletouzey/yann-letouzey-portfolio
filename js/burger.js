5c889ff530f8169eb0f550545d17f20b32b73642
Yann Letouzey <y.letouzey@gmail.com>
2023-05-20 15:17:29 +0200

    add transition menu

--- js/burger.js
+++ js/burger.js
@@ -2,9 +2,37 @@ export function burgerActive() {
     const header__container = document.getElementById('header__container')
     const header__icon = document.getElementById('header__icon')
     const header__contact = document.getElementById('header__contact')
+    const header__title = document.getElementById('header__title')
+
     header__icon.addEventListener('click', () => {
-        header__container.classList.toggle('is-opened')
-        header__icon.classList.toggle('is-opened')
-        header__contact.classList.toggle('is-opened')
+        if (header__container.classList.contains('is-opened')) {
+            header__container.classList.remove('is-opened')
+            header__icon.classList.remove('is-opened')
+            header__contact.classList.remove('is-opened')
+            header__title.classList.remove('is-opened')
+            header__container.classList.add('is-closed')
+            header__icon.classList.add('is-closed')
+            header__contact.classList.add('is-closed')
+            header__title.classList.add('is-closed')
+        } else {
+            header__container.classList.add('is-opened')
+            header__icon.classList.add('is-opened')
+            header__contact.classList.add('is-opened')
+            header__title.classList.add('is-opened')
+            header__container.classList.remove('is-closed')
+            header__icon.classList.remove('is-closed')
+            header__contact.classList.remove('is-closed')
+            header__title.classList.remove('is-closed')
+        }
     })
+    // window.addEventListener('resize', () => {
+    //     if (header__contact.classList.contains('is-opened')) {
+    //         if (window.innerWidth < 530) {
+    //             header__title.style.transform = `translateX(-${header__contact.clientWidth}px);`
+    //         } else {
+    //             header__title.style.transitionDuration = "0.5s"
+    //             header__title.style.transform = `translateX(0);`
+    //         }
+    //     }
+    // })
 }
