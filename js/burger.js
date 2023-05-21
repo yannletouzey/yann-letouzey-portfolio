e54cf5ede78e3d468b549980684fc0390520e120
Yann Letouzey <y.letouzey@gmail.com>
2023-05-21 13:19:44 +0200

    add animation title click burger

--- js/burger.js
+++ js/burger.js
@@ -2,27 +2,32 @@ export function burgerActive() {
     const header__container = document.getElementById('header__container')
     const header__icon = document.getElementById('header__icon')
     const header__contact = document.getElementById('header__contact')
-    const header__title = document.getElementById('header__title')
+    const firstname = document.querySelector('.header__title--firstname')
+    const lastname = document.getElementById('header__title--lastname')
 
     header__icon.addEventListener('click', () => {
         if (header__container.classList.contains('is-opened')) {
             header__container.classList.remove('is-opened')
             header__icon.classList.remove('is-opened')
             header__contact.classList.remove('is-opened')
-            header__title.classList.remove('is-opened')
             header__container.classList.add('is-closed')
             header__icon.classList.add('is-closed')
             header__contact.classList.add('is-closed')
-            header__title.classList.add('is-closed')
+            lastname.classList.remove('lastnameHide')
+            lastname.classList.add('lastnameShow')
+            firstname.classList.remove('firstnameHide')
+            firstname.classList.add('firstnameShow')
         } else {
+            lastname.classList.add('lastnameHide')
+            lastname.classList.remove('lastnameShow')
+            firstname.classList.add('firstnameHide')
+            firstname.classList.remove('firstnameShow')
             header__container.classList.add('is-opened')
             header__icon.classList.add('is-opened')
             header__contact.classList.add('is-opened')
-            header__title.classList.add('is-opened')
             header__container.classList.remove('is-closed')
             header__icon.classList.remove('is-closed')
             header__contact.classList.remove('is-closed')
-            header__title.classList.remove('is-closed')
         }
     })
 }
