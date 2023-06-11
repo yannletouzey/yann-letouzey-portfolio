84fd19fe42e606a89e9512706a8e055f38d0607b
Yann Letouzey <y.letouzey@gmail.com>
2023-06-11 21:35:26 +0200

    fix bug

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ import { utils } from "./utils";
 import { burger } from "./burger";
 import { animateStart } from "./animateStart";
 carousel();
-utils.spanText()
-utils.addSpanReverse('banner__panel')
-burger()
-animateStart()
+utils.spanText();
+utils.addSpanReverse("banner__panel");
+burger();
+animateStart();
--- js/utils.js
+++ js/utils.js
@@ -1,35 +1,37 @@
 export const utils = {
-    addSpan: function (id) {
-        const textWithId = document.querySelector('.' + id)
-        const text = document.querySelector('.' + id).textContent
-        textWithId.innerHTML = ""
-        let textArray = Array.from(text)
-        for (let i = 0; i < textArray.length; i++) {
-            let e = textArray[i];
-            textWithId.innerHTML += `<span id=${i}>${e}</span>`
-        }
-    },
-    spanText: function () {
-        const spanText = document.querySelectorAll(".container__carousel--link div");
-        spanText.forEach((element) => {
-            const letters = element.children[0].textContent.split("");
-            element.innerHTML = "";
-            letters.forEach((e, index) => {
-                element.innerHTML += `<span style="display: inline-block; transition: 0.5s; transition-delay: ${
-                    0.02 * index
-                }s">${e}</span>`;
-            });
-        });
-    },
-    addSpanReverse: function  (id) {
-        const textWithId = document.getElementById('' + id)
-        let text = document.getElementById('' + id).textContent
-        text = [...text].reverse().join("");
-        textWithId.innerHTML = ""
-        let textArray = Array.from(text)
-        for (let i = 0; i < textArray.length; i++) {
-            let e = textArray[i];
-            textWithId.innerHTML += `<span id=${i}>${e}</span>`
-        }
+  addSpan: function (id) {
+    const textWithId = document.querySelector("." + id);
+    const text = document.querySelector("." + id).textContent;
+    textWithId.innerHTML = "";
+    let textArray = Array.from(text);
+    for (let i = 0; i < textArray.length; i++) {
+      let e = textArray[i];
+      textWithId.innerHTML += `<span id=${i}>${e}</span>`;
     }
-}
+  },
+  spanText: function () {
+    const spanText = document.querySelectorAll(
+      ".container__carousel--link div"
+    );
+    spanText.forEach((element) => {
+      const letters = element.children[0].textContent.split("");
+      element.innerHTML = "";
+      letters.forEach((e, index) => {
+        element.innerHTML += `<span style="display: inline-block; transition: 0.5s; transition-delay: ${
+          0.02 * index
+        }s">${e}</span>`;
+      });
+    });
+  },
+  addSpanReverse: function (id) {
+    const textWithId = document.getElementById("" + id);
+    let text = document.getElementById("" + id).textContent;
+    text = [...text].reverse().join("");
+    textWithId.innerHTML = "";
+    let textArray = Array.from(text);
+    for (let i = 0; i < textArray.length; i++) {
+      let e = textArray[i];
+      textWithId.innerHTML += `<span id=${i}>${e}</span>`;
+    }
+  },
+};
