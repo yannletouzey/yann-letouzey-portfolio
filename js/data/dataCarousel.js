53f8908c56fb8aedc821d36b8fd8cb9c8c9e72e1
Yann Letouzey <y.letouzey@gmail.com>
2023-08-11 19:40:40 +0200

    delete projects + change img build-cube

--- js/data/dataCarousel.js
+++ js/data/dataCarousel.js
@@ -8,7 +8,7 @@ export const dataCarousel = [
             html: new URL("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg", import.meta.url),
             css: new URL("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg", import.meta.url)
         },
-        stack: "Front-end",
+        stack: "front-end",
         inline: true
     },
     {
@@ -21,25 +21,11 @@ export const dataCarousel = [
             css: new URL("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg", import.meta.url),
             js: new URL("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg", import.meta.url),
         },
-        stack: "Front-end",
+        stack: "front-end",
         inline: true
     },
     {
         id: 3,
-        imgUrl: new URL("../../src/img/screenshot-website/threejs-game.png", import.meta.url),
-        title: "threejs-game",
-        link: "https://threejs-game-tawny.vercel.app/",
-        techno: {
-            html: new URL("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg", import.meta.url),
-            css: new URL("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg", import.meta.url),
-            js: new URL("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg", import.meta.url),
-            three: new URL("../../src/img/logos/three.png", import.meta.url)
-        },
-        stack: "Front-end",
-        inline: true
-    },
-    {
-        id: 4,
         imgUrl: new URL("../../src/img/screenshot-website/screenshot-yzey-3d.png", import.meta.url),
         title: "webgl-3d-three.js",
         link: "https://webgl-3d-text-five-tau.vercel.app/",
@@ -49,11 +35,11 @@ export const dataCarousel = [
             js: new URL("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg", import.meta.url),
             three: new URL("../../src/img/logos/three.png", import.meta.url)
         },
-        stack: "Front-end",
+        stack: "front-end",
         inline: true
     },
     {
-        id: 5,
+        id: 4,
         imgUrl: new URL("../../src/img/screenshot-website/haunted-house.png", import.meta.url),
         title: "haunted-house",
         link: "https://haunted-house-blond-xi.vercel.app/",
@@ -63,24 +49,11 @@ export const dataCarousel = [
             js: new URL("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg", import.meta.url),
             three: new URL("../../src/img/logos/three.png", import.meta.url)
         },
-        stack: "Front-end",
+        stack: "front-end",
         inline: true
     },
     {
-        id: 6,
-        imgUrl: new URL("../../src/img/screenshot-website/particle.png", import.meta.url),
-        title: "particle.vercel",
-        link: "https://particle-canvas.vercel.app/",
-        techno: {
-            html: new URL("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg", import.meta.url),
-            css: new URL("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg", import.meta.url),
-            js: new URL("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg", import.meta.url)
-        },
-        stack: "Front-end",
-        inline: true
-    },
-    {
-        id: 7,
+        id: 5,
         imgUrl: new URL("../../src/img/screenshot-website/1440x900.png", import.meta.url),
         title: "easy-time-clock",
         link: "#",
@@ -91,11 +64,11 @@ export const dataCarousel = [
             php: new URL("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg", import.meta.url),
             mysql: new URL("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg", import.meta.url)
         },
-        stack: "Back-end",
+        stack: "back-end",
         inline: false
     },
     {
-        id: 8,
+        id: 6,
         imgUrl: new URL("../../src/img/screenshot-website/home-large.png", import.meta.url),
         title: "statistiques-covid",
         link: "https://www.statistiques-covid.com",
@@ -106,11 +79,11 @@ export const dataCarousel = [
             php: new URL("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg", import.meta.url),
             mysql: new URL("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg", import.meta.url)
         },
-        stack: "Back-end",
+        stack: "back-end",
         inline: true
     },
     {
-        id: 9,
+        id: 7,
         imgUrl: new URL("../../src/img/screenshot-website/bounce-ball.png", import.meta.url),
         title: "bounce-ball",
         link: "https://bounce-ball.vercel.app",
@@ -119,11 +92,11 @@ export const dataCarousel = [
             css: new URL("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg", import.meta.url),
             js: new URL("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg", import.meta.url)
         },
-        stack: "Front-end",
+        stack: "front-end",
         inline: true
     },
     {
-        id: 10,
+        id: 8,
         imgUrl: new URL("../../src/img/screenshot-website/cube-in-cube.png", import.meta.url),
         title: "cube-in-cube",
         link: "https://cube-in-cube.vercel.app",
@@ -132,11 +105,11 @@ export const dataCarousel = [
             css: new URL("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg", import.meta.url),
             js: new URL("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg", import.meta.url)
         },
-        stack: "Front-end",
+        stack: "front-end",
         inline: true
     },
     {
-        id: 11,
+        id: 9,
         imgUrl: new URL("../../src/img/screenshot-website/visuel-padding-margin.png", import.meta.url),
         title: "padding-margin-css",
         link: "https://visual-padding-margin-css.vercel.app/",
@@ -145,12 +118,12 @@ export const dataCarousel = [
             css: new URL("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg", import.meta.url),
             js: new URL("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg", import.meta.url)
         },
-        stack: "Front-end",
+        stack: "front-end",
         inline: true
     },
     {
-        id: 12,
-        imgUrl: new URL("../../src/img/gif/build-cube-css.gif", import.meta.url),
+        id: 10,
+        imgUrl: new URL("../../src/img/screenshot-website/build-cube-css.png", import.meta.url),
         title: "build-cube-css",
         link: "https://build-cube-css.vercel.app/",
         techno: {
@@ -158,11 +131,11 @@ export const dataCarousel = [
             css: new URL("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg", import.meta.url),
             js: new URL("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg", import.meta.url)
         },
-        stack: "Front-end",
+        stack: "front-end",
         inline: true
     },
     {
-        id: 13,
+        id: 11,
         imgUrl: new URL("../../src/img/screenshot-website/cardiac-cohesion.png", import.meta.url),
         title: "cardiac-cohesion",
         link: "https://cardiac-cohesion.vercel.app/",
@@ -171,7 +144,7 @@ export const dataCarousel = [
             css: new URL("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg", import.meta.url),
             js: new URL("https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg", import.meta.url)
         },
-        stack: "Front-end",
+        stack: "front-end",
         inline: true
     }
 ]
