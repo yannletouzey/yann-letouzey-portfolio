acdf3fec64d55d5762421fab909a342409000142
Yann Letouzey <y.letouzey@gmail.com>
2023-09-08 22:09:26 +0200

    create container html with js

--- js/carousel.js
+++ js/carousel.js
@@ -1,31 +1,97 @@
 import { dataCarousel } from "./data/dataCarousel";
 import { spin } from "./spin";
-console.log(dataCarousel.length);
 
 export const carousel = () => {
   const prevBtn = document.getElementById("prev");
   const nextBtn = document.getElementById("next");
   const carousel = document.getElementById("container__carousel");
-  const allFaces = document.querySelectorAll(".container__carousel--face");
   const offsetFace = ((carousel.clientWidth / 2) / 16);
-  const degValue = 90; // 1/4 of lap
+  const degValue = 90;
   let degrees = 0
   let current = 1
-//  cree ce html en js
-{/* <div id="container__carousel" class="container__carousel">
-      <div class="container__carousel--face" id="container__carousel--face"></div>
-      <div class="container__carousel--face"></div>
-      <div class="container__carousel--face"></div>
-      <div class="container__carousel--face"></div>
-      <div class="container__carousel--face hide"></div>
-      <div class="container__carousel--face hide"></div>
-      <div class="container__carousel--face hide"></div>
-      <div class="container__carousel--face hide"></div>
-      <div class="container__carousel--face hide"></div>
-      <div class="container__carousel--face hide"></div>
-      <div class="container__carousel--face hide"></div>
-      <div class="container__carousel-glow-ground" id="container__carousel-glow-ground"></div>
-  </div> */}
+
+  for (let index = 0; index < dataCarousel.length; index++) {
+    const container__carousel__face = document.createElement("div");
+    container__carousel__face.classList.add("container__carousel--face");
+    if (index === 0) {
+      container__carousel__face.setAttribute("id", "container__carousel--face");
+    }
+    if (index >= 4) {
+      container__carousel__face.classList.add("hide");
+    }
+    carousel.appendChild(container__carousel__face);
+  }
+  const container__carousel__face = document.createElement("div");
+  container__carousel__face.classList.add("container__carousel-glow-ground");
+  container__carousel__face.setAttribute("id", "container__carousel-glow-ground");
+  carousel.appendChild(container__carousel__face);
+
+  const allFaces = document.querySelectorAll(".container__carousel--face");  
+
+  const technoImg = ['html','css','js','three','mysql','php','node','express']
+  for (let index = 0; index < dataCarousel.length; index++) {
+    const face = allFaces[index];
+    const newDegValue = degValue * index;
+    face.style.transform = `rotateY(${newDegValue}deg) translateZ(${offsetFace}rem)`;
+
+    // Create div container => img 
+    const boxImg = document.createElement('div')
+    boxImg.classList.add('container__carousel--img')
+
+    // Create img
+    const img = document.createElement('img')
+    img.src = dataCarousel[index].imgUrl
+    boxImg.append(img)
+
+    // Create div container => desc 
+    const boxDesc = document.createElement('div')
+    boxDesc.classList.add('container__carousel--desc')
+
+    // Create link
+    const linkDiv = document.createElement('div')
+    linkDiv.classList.add('container__carousel--link')
+
+    // Create div/span for rotate animation text
+    for (let i = 1; i <= 2; i++) {
+      const link = document.createElement('a')
+      const div = document.createElement('div')
+      div.className = `containerLink s${i}`
+      const span = document.createElement('span')
+      div.append(span)
+      link.append(div)
+      const textLink = document.createTextNode(dataCarousel[index].title)
+      link.href = dataCarousel[index].link
+      span.append(textLink)
+      linkDiv.append(link) 
+    }
+
+    // Create box img techno
+    const technoBox =  document.createElement('div')
+    technoBox.classList.add('container__carousel--techno')
+    technoImg.forEach((imgTechno, i) => {
+      if (dataCarousel[index].techno[imgTechno]) {
+        const img = document.createElement('img')
+        img.src = dataCarousel[index].techno[imgTechno]
+        technoBox.append(img)
+      }
+    })
+
+    // create tag stack
+    const tagStack = document.createElement('div')
+    tagStack.classList.add('container__carousel--tagStack')
+    tagStack.textContent = dataCarousel[index].stack
+    
+    if (!dataCarousel[index].inline) {
+      const tooltip = document.createElement('div')
+      tooltip.classList.add('container__carousel--tooltip')
+      tooltip.textContent = `${dataCarousel[index].title} n'est pas encore en ligne`
+      boxDesc.prepend(tooltip)
+    }
+
+    boxDesc.append(linkDiv, technoBox, tagStack)
+    face.append(boxImg, boxDesc)
+  }
+
   prevBtn.addEventListener("click", () => {
     if (current > 1) {
       current--
@@ -64,68 +130,4 @@ export const carousel = () => {
       })
     }
   });
-
-  const technoImg = ['html','css','js','three','mysql','php','node','express']
-  for (let index = 0; index < allFaces.length; index++) {
-    const face = allFaces[index];
-    const newDegValue = degValue * index;
-    face.style.transform = `rotateY(${newDegValue}deg) translateZ(${offsetFace}rem)`;
-
-    // Create div container => img 
-    const boxImg = document.createElement('div')
-    boxImg.classList.add('container__carousel--img')
-    
-    // Create img
-    const img = document.createElement('img')
-    img.src = dataCarousel[index].imgUrl
-    boxImg.append(img)
-    
-    // Create div container => desc 
-    const boxDesc = document.createElement('div')
-    boxDesc.classList.add('container__carousel--desc')
-    
-    // Create link
-    const linkDiv = document.createElement('div')
-    linkDiv.classList.add('container__carousel--link')
-    
-    // Create div/span for rotate animation text
-    for (let i = 1; i <= 2; i++) {
-      const link = document.createElement('a')
-      const div = document.createElement('div')
-      div.classList.add('containerLink', 's' + i)
-      const span = document.createElement('span')
-      div.append(span)
-      link.append(div)
-      const textLink = document.createTextNode(dataCarousel[index].title)
-      link.href = dataCarousel[index].link
-      span.append(textLink)
-      linkDiv.append(link) 
-    }
-
-    // Create box img techno
-    const technoBox =  document.createElement('div')
-    technoBox.classList.add('container__carousel--techno')
-    technoImg.forEach((imgTechno, i) => {
-      if (dataCarousel[index].techno[imgTechno]) {
-        const img = document.createElement('img')
-        img.src = dataCarousel[index].techno[imgTechno]
-        technoBox.append(img)
-      }
-    })
-
-    // create tag stack
-    const tagStack = document.createElement('div')
-    tagStack.classList.add('container__carousel--tagStack')
-    tagStack.textContent = dataCarousel[index].stack
-    
-    if (!dataCarousel[index].inline) {
-      const tooltip = document.createElement('div')
-      tooltip.classList.add('container__carousel--tooltip')
-      tooltip.textContent = `${dataCarousel[index].title} n'est pas encore en ligne`
-      boxDesc.prepend(tooltip)
-    }
-
-    boxDesc.append(linkDiv, technoBox, tagStack)
-    face.append(boxImg, boxDesc)
-  }
 };
--- js/particle.js
+++ js/particle.js
@@ -1,184 +1,166 @@
-import strokeColor from './stroke.js';
-import color from './color.js';
-import lineWidth from './lineWidth.js';
-import setting from './setting.js'
+export const particle = () => {
+    const colorFillStyle = window.getComputedStyle( document.querySelector('body'), null).getPropertyValue( 'background-color' );
+    const canvas = document.querySelector('.canvas')
+    const ctx = canvas.getContext('2d')
+    const radius = 30
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+    ctx.strokeStyle = `#2c2c2c96`
+    ctx.lineWidth = 1
+    ctx.fillStyle = `${colorFillStyle}`
+    // ctx.fillStyle = `${colorFillStyle}`
 
-
-const canvas = document.getElementById('canvas')
-const ctx = canvas.getContext('2d')
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
-
-lineWidth(ctx)
-color(ctx)
-strokeColor(ctx)
-setting()
-
-class Particle {
-    constructor(effect, index){
-        this.effect = effect
-        this.index = index
-        this.radius = Math.random() * 20 + (10 / 2)
-        this.x = this.radius + Math.random() * (this.effect.width - this.radius * 2)
-        this.y = this.radius + Math.random() * (this.effect.height - this.radius * 2)
-        this.velocityX = Math.random() * 1 - 0.5
-        this.velocityY = Math.random() * 1 - 0.5
-        this.pushX = 0
-        this.pushY = 0
-        this.friction = 0.8
-    }
-    draw(context){
-        if (this.effect.mouse.pressed) {
-            const distanceX = Math.abs(this.effect.mouse.x - this.x)
-            const distanceY = Math.abs(this.effect.mouse.y - this.y)
-            const distanceMax = 150
-            if (distanceX < distanceMax && distanceY < distanceMax) {
-                context.beginPath()
-                context.moveTo(this.x, this.y)
-                context.lineTo(this.effect.mouse.x, this.effect.mouse.y)
-                context.fill()
-                context.stroke()
+    class Particle {
+        constructor(effect, index){
+            this.effect = effect
+            this.index = index
+            this.radius = Math.random() * radius + (radius / 4)
+            this.x = this.radius + Math.random() * (this.effect.width - this.radius * 2)
+            this.y = this.radius + Math.random() * (this.effect.height - this.radius * 2)
+            this.velocityX = Math.random() * 1 - 0.5
+            this.velocityY = Math.random() * 1 - 0.5
+            this.pushX = 0
+            this.pushY = 0
+            this.friction = 0.8
+        }
+        draw(context){
+            if (this.effect.mouse.pressed) {
+                const distanceX = Math.abs(this.effect.mouse.x - this.x)
+                const distanceY = Math.abs(this.effect.mouse.y - this.y)
+                const distanceMax = 150
+                if (distanceX < distanceMax && distanceY < distanceMax) {
+                    context.beginPath()
+                    context.moveTo(this.x, this.y)
+                    context.lineTo(this.effect.mouse.x, this.effect.mouse.y)
+                    context.fill()
+                    context.stroke()
+                }
+            }
+            context.beginPath()
+            context.arc(this.x, this.y, this.radius, 0, Math.PI * 2)
+            context.fill()
+            context.stroke()
+        }
+        update(){
+            if (this.effect.mouse.pressed) {
+                const dx = this.x - this.effect.mouse.x
+                const dy = this.y - this.effect.mouse.y
+                const distance = Math.hypot(dx, dy)
+                const force = (this.effect.mouse.radius / distance)
+                if (distance < this.effect.mouse.radius) {
+                    const angle = Math.atan2(dy, dx)
+                    this.pushX += Math.cos(angle) * force
+                    this.pushY += Math.sin(angle) * force
+                }
+            }
+            this.x += (this.pushX *= this.friction) + this.velocityX
+            this.y += (this.pushY *= this.friction) + this.velocityY
+            if (this.x < this.radius) {
+                this.x = this.radius
+                this.velocityX *= -1
+            } else if (this.x > this.effect.width - this.radius){
+                this.x = this.effect.width - this.radius
+                this.velocityX *= -1
+            }
+            if (this.y < this.radius) {
+                this.y = this.radius
+                this.velocityY *= -1
+            } else if (this.y > this.effect.height - this.radius){
+                this.y = this.effect.height - this.radius
+                this.velocityY *= -1
             }
         }
-        context.beginPath()
-        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2)
-        context.fill()
-        context.stroke()
-    }
-    update(){
-        if (this.effect.mouse.pressed) {
-            const dx = this.x - this.effect.mouse.x
-            const dy = this.y - this.effect.mouse.y
-            const distance = Math.hypot(dx, dy)
-            const force = (this.effect.mouse.radius / distance)
-            if (distance < this.effect.mouse.radius) {
-                const angle = Math.atan2(dy, dx)
-                this.pushX += Math.cos(angle) * force
-                this.pushY += Math.sin(angle) * force
-            }
-        }
-        this.x += (this.pushX *= this.friction) + this.velocityX
-        this.y += (this.pushY *= this.friction) + this.velocityY
-        if (this.x < this.radius) {
-            this.x = this.radius
-            this.velocityX *= -1
-        } else if (this.x > this.effect.width - this.radius){
-            this.x = this.effect.width - this.radius
-            this.velocityX *= -1
-        }
-        if (this.y < this.radius) {
-            this.y = this.radius
-            this.velocityY *= -1
-        } else if (this.y > this.effect.height - this.radius){
-            this.y = this.effect.height - this.radius
-            this.velocityY *= -1
+        reset(){
+            this.x = this.radius + Math.random() * (this.effect.width - this.radius * 2)
+            this.y = this.radius + Math.random() * (this.effect.height - this.radius * 2)
         }
     }
-    reset(){
-        this.x = this.radius + Math.random() * (this.effect.width - this.radius * 2)
-        this.y = this.radius + Math.random() * (this.effect.height - this.radius * 2)
-    }
-}
 
-class Effect {
-    constructor(canvas, context){
-        this.canvas = canvas
-        this.context = context
-        this.width = this.canvas.width
-        this.height = this.canvas.height
-        this.particles = []
-        this.numberOfParticles = 100
-        this.createParticle()
-        window.addEventListener('resize', (e) => {
-            this.resize(e.target.innerWidth, e.target.innerHeight)
-        })
-        this.mouse = {
-            x: 0,
-            y: 0,
-            pressed: false,
-            radius: 100
-        }
-        window.addEventListener('mousemove', (e) => {
-            if (this.mouse.pressed) {
+    class Effect {
+        constructor(canvas, context){
+            this.canvas = canvas
+            this.context = context
+            this.width = this.canvas.width
+            this.height = this.canvas.height
+            this.particles = []
+            this.numberOfParticles = 50
+            this.createParticle()
+            window.addEventListener('resize', (e) => {
+                this.resize(e.target.innerWidth, e.target.innerHeight)
+            })
+            this.mouse = {
+                x: 0,
+                y: 0,
+                pressed: false,
+                radius: 100
+            }
+            window.addEventListener('mousemove', (e) => {
+                if (this.mouse.pressed) {
+                    this.mouse.x = e.x
+                    this.mouse.y = e.y  
+                }
+            })
+            window.addEventListener('mousedown', (e) => {
+                this.mouse.pressed = true
+                this.mouse.x = e.x
+                this.mouse.y = e.y
+            })
+            window.addEventListener('mouseup', (e) => {
+                this.mouse.pressed = false
                 this.mouse.x = e.x
                 this.mouse.y = e.y  
-            }
-        })
-        window.addEventListener('mousedown', (e) => {
-            this.mouse.pressed = true
-            this.mouse.x = e.x
-            this.mouse.y = e.y  
-        })
-        window.addEventListener('mouseup', (e) => {
-            this.mouse.pressed = false
-            this.mouse.x = e.x
-            this.mouse.y = e.y  
-        })
-    }
-    createParticle(){
-        for (let p = 0; p < this.numberOfParticles; p++) {
-            this.particles.push(new Particle(this, p))
+            })
         }
-    }
-    handleParticle(context){
-        this.connectParticle(context)
-        this.particles.forEach(p => {
-            p.draw(context)
-            p.update()
-        })
-    }
-    connectParticle(context){
-        const maxDistance = 100
-        for (let i = 0; i < this.particles.length; i++) {
-            for (let j = 0; j < this.particles.length; j++) {
-                const distanceX = this.particles[i].x - this.particles[j].x
-                const distanceY = this.particles[i].y - this.particles[j].y
-                const distance = Math.hypot(distanceX, distanceY)
-                if (distance < maxDistance) {
-                    context.save()
-                    const opacity = 1 - (distance / maxDistance)
-                    context.globalAlpha = opacity
-                    context.beginPath()
-                    context.moveTo(this.particles[i].x, this.particles[i].y)
-                    context.lineTo(this.particles[j].x, this.particles[j].y)
-                    context.stroke()
-                    context.restore()
+        createParticle(){
+            for (let p = 0; p < this.numberOfParticles; p++) {
+                this.particles.push(new Particle(this, p))
+            }
+        }
+        handleParticle(context){
+            this.connectParticle(context)
+            this.particles.forEach(p => {
+                p.draw(context)
+                p.update()
+            })
+        }
+        connectParticle(context){
+            const maxDistance = 150
+            for (let i = 0; i < this.particles.length; i++) {
+                for (let j = 0; j < this.particles.length; j++) {
+                    const distanceX = this.particles[i].x - this.particles[j].x
+                    const distanceY = this.particles[i].y - this.particles[j].y
+                    const distance = Math.hypot(distanceX, distanceY)
+                    if (distance < maxDistance) {
+                        context.save()
+                        const opacity = 1 - (distance / maxDistance)
+                        context.globalAlpha = opacity
+                        context.beginPath()
+                        context.moveTo(this.particles[i].x, this.particles[i].y)
+                        context.lineTo(this.particles[j].x, this.particles[j].y)
+                        context.stroke()
+                        context.restore()
+                    }
                 }
             }
         }
+        resize(width, height){
+            this.canvas.width = this.width
+            this.canvas.height = this.height
+            this.width = width
+            this.height = height
+            ctx.strokeStyle = '#ceafafb5'
+            ctx.lineWidth = 0.3
+            ctx.fillStyle = '#0c0c0c'
+            this.particles.forEach(particle => {
+                particle.reset()
+            })
+        }
     }
-    resize(width, height){
-        this.canvas.width = this.width
-        this.canvas.height = this.height
-        this.width = width
-        this.height = height
-        // strokeColor(ctx)
-        // color(ctx)
-        // lineWidth(ctx)
-        this.particles.forEach(particle => {
-            particle.reset()
-        })
+    let effect = new Effect(canvas, ctx)
+    function animate() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        effect.handleParticle(ctx)
+        requestAnimationFrame(animate)
     }
+    animate()
 }
-// const rangeParticle = document.getElementById('nbrParticle')
-// let nbrParticle = rangeParticle.value
-
-// const rangeSizeParticle = document.getElementById('sizeParticle')
-// let sizeParticle = rangeSizeParticle.value
-
-let effect = new Effect(canvas, ctx)
-
-// rangeParticle.oninput = function() {
-//     nbrParticle = this.value;
-//     effect = new Effect(canvas, nbrParticle, ctx, sizeParticle )
-// }
-// rangeSizeParticle.oninput = function() {
-//     sizeParticle = this.value;
-//     effect = new Effect(canvas, nbrParticle, ctx, sizeParticle )
-// }
-function animate() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    effect.handleParticle(ctx)
-    requestAnimationFrame(animate)
-}
-animate()
